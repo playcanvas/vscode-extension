b36ad263bb29b04ad15ff4d9413feaf2ff79440b
Iakov Sumygin <isumygin@snapchat.com>
2023-07-25 15:10:47 +0100

    drop treeview

--- src/cloudStorageProvider.js
+++ src/cloudStorageProvider.js
@@ -1,13 +1,7 @@
 const vscode = require('vscode');
 const Api = require('./api');
-const { get } = require('http');
 const path = require('path');
 
-// local
-// const defaultToken = 'cFT9ubWG5YHvGToNQoQi37KOLTY3yyRL';
-// dev
-const defaultToken = 'ycokBkYiHO5o4nsMTEOYtMD05BAQ7lrW';
-const defaultUser = 'yakov-snap';
 
 class CloudStorageProvider {
     constructor() {
@@ -15,28 +9,49 @@ class CloudStorageProvider {
         this.projects = [];
         this.userId = null;
         this.content = new Map();
-
+        this.currentProject = null;
+        this._onDidChangeFile = new vscode.EventEmitter();
         this.refresh();
     }
 
-    onDidChangeFile() {
-        console.log('onDidChangeFile');
+    get onDidChangeFile() {
+        console.log('playcanvas: onDidChangeFile');
+        return this._onDidChangeFile.event;
     }
 
-    async preloadProjectFiles(uri) {
+    async refreshProjectFiles(uri) {
+        console.log(`playcanvas: refreshProjectFiles ${uri.path}`);
+
         // special case - the list of projects is not loaded yet
         const projects = await this.fetchProjects();
         // extract project name from url
         const projectName = uri.path.split('/')[1];
         const project = projects.find(p => p.name === projectName);
         if (project) {
+            // force refresh the file list
+            delete project.files;
             await this.fetchFiles(project);
         }
     }
 
     async stat(uri) {
+        console.log(`playcanvas: stat ${uri.path}`);
+        
+        await this.fetchProjects();
+        const project = this.getProject(uri.path);
+        if (!project) {
+            throw vscode.FileSystemError.FileNotFound();
+        }
+
+        const projectModified = new Date(project.modified).getTime();
+        const projectCreated = new Date(project.created).getTime();
+
+        if (uri.path === `/${project.name}`) {
+            return { type: vscode.FileType.Directory, permissions: 0, size: 0, ctime: projectCreated, mtime: projectModified };
+        }
+
         if (!this.getFileData(uri)) {
-            await this.preloadProjectFiles(uri);
+            await this.refreshProjectFiles(uri);
         }
 
         const fileData = this.getFileData(uri);
@@ -44,32 +59,39 @@ class CloudStorageProvider {
             throw vscode.FileSystemError.FileNotFound();
         }
 
-        if (!fileData || fileData.type === 'folder') {
-            return { type: vscode.FileType.Directory, size: 0, ctime: 0, mtime: 0 };
+        const modified = new Date(fileData.modifiedAt).getTime();
+        const created = new Date(fileData.createdAt).getTime();
+
+        if (!fileData) {
+            throw vscode.FileSystemError.FileNotFound();
+        } 
+        
+        if (fileData.type === 'folder') {
+            return { type: vscode.FileType.Directory, permissions: 0, size: 0, ctime: created, mtime: modified };
         }
-        if (!fileData.content) {
-            const project = this.getProject(uri.path);
-            fileData.content = await this.fetchFileContent(fileData, project.branchId);
-        }
-        if (fileData.content !== null) {
-            return { type: vscode.FileType.File, size: fileData.content.length, ctime: 0, mtime: 0 };
-        }
-        throw vscode.FileSystemError.FileNotFound();
-    }
+
+        return { type: vscode.FileType.File, permissions: 0, size: fileData.file.size, ctime: created, mtime: modified };
+      }
 
     async readFile(uri) {
+        console.log(`playcanvas: readFile ${uri.path}`);
+
+        const project = this.getProject(uri.path);
+        if (!project) {
+            throw vscode.FileSystemError.FileNotFound();
+        }
+
         if (!this.getFileData(uri)) {
-            await this.preloadProjectFiles(uri);
+            await this.refreshProjectFiles(uri);
         }
 
         const fileData = this.getFileData(uri);
         if (!fileData || fileData.type === 'folder') {
             return new Uint8Array();
         }
-        if (!fileData.content) {
-            const project = this.getProject(uri.path);
-            fileData.content = await this.fetchFileContent(fileData, project.branchId);
-        }
+        // if (!fileData.content) {
+        fileData.content = await this.fetchFileContent(fileData, project.branchId);
+        // }
 
         if (fileData.content === null) {       
             throw vscode.FileSystemError.FileNotFound();
@@ -77,19 +99,49 @@ class CloudStorageProvider {
         return new TextEncoder().encode(fileData.content);
     }    
 
-    async writeFile(uri, content) {
-        const fileData = this.getFileData(uri);
+    async writeFile(uri, content, options) {
+        console.log(`playcanvas: writeFile ${uri.path}`);
+
         const project = this.getProject(uri.path);
-        const asset = await this.api.uploadFile(fileData.id, fileData.file.filename, fileData.modifiedAt, project.branchId, content);
-        fileData.modifiedAt = asset.modifiedAt;
-        fileData.content = new TextDecoder().decode(content);
+        const fileData = this.getFileData(uri);
+        if (!fileData) {
+
+            if (!options.create) {
+                throw vscode.FileSystemError.FileNotFound();
+            }
+
+            const folderPath = path.dirname(uri.path);
+            const folderUri = vscode.Uri.parse(`playcanvas:${path.dirname(uri.path)}`);
+            
+            // Construct the new Uri using the folder path and new name
+            try {
+                const root = folderPath === `/${project.name}`;
+                const folderData = root ? null : this.getFileData(folderUri);
+                const name = uri.path.split('/').pop();
+                await this.api.createAsset(project.id, project.branchId, folderData ? folderData.id : null, name);
+            } catch (error) {
+                vscode.window.showErrorMessage(`Failed to create a file: ${error.message}`);
+            }
+        } else {
+            const asset = await this.api.uploadFile(fileData.id, fileData.file.filename, fileData.modifiedAt, project.branchId, content);
+            fileData.modifiedAt = asset.modifiedAt;
+            fileData.content = new TextDecoder().decode(content);
+        }
     }
 
-    watch(uri, options) {
-        console.log('watch', uri, options);
+
+	watch(uri) {
+		// ignore, fires for all changes...
+		return new vscode.Disposable(() => { });
+	}
+
+    isWritableFileSystem(scheme) {
+        return true;
     }
     
     async rename(oldUri, newUri) {
+        console.log(`playcanvas: rename ${oldUri.path}`);
+
         const newName = newUri.path.split('/').pop();
         const fileData = this.getFileData(oldUri);
         const project = this.getProject(oldUri.path);
@@ -111,8 +163,10 @@ class CloudStorageProvider {
     } 
 
     async copy(sourceUri, targetUri) {
+        console.log(`playcanvas: copy ${sourceUri.path}`);
+
         const fileData = this.getFileData(sourceUri);
-        const folderUri = vscode.Uri.parse(`playcanvas:/${path.dirname(targetUri.path)}`);
+        const folderUri = vscode.Uri.parse(`playcanvas:${path.dirname(targetUri.path)}`);
         const folderData = this.getFileData(folderUri);
         const sourceProject = this.getProject(sourceUri.path);
         const targetProject = this.getProject(targetUri.path);
@@ -121,27 +175,44 @@ class CloudStorageProvider {
             targetProject.id, targetProject.branchId, folderId);        
     }
 
-    async createFile(name, folderUri, type) {
-        const project = this.getProject(folderUri.path);
-        const folderData = this.getFileData(folderUri);
-        await this.api.createAsset(project.id, project.branchId, folderData ? folderData.id : null, name, type);
-    }
-
     async delete(uri) {
         const fileData = this.getFileData(uri);
         const project = this.getProject(uri.path);
         await this.api.deleteAsset(fileData.id, project.branchId);
     }
 
-    readDirectory(uri) {
-        console.log('readDirectory', uri);
+    async readDirectory(uri) {
+        console.log(`playcanvas: readDirectory ${uri.path}`);
+        const project = this.getProject(uri.path);
+        const files = await this.fetchFiles(project);
+        const file = this.getFileData(uri);
+        const parentId = file ? file.id : null;
+        const folderFiles = files.filter(f => f.parent === parentId);
+        return folderFiles.map(f => [f.name, f.type == 'folder' ? vscode.FileType.Directory : vscode.FileType.File]);
     }
 
-    createDirectory(uri) {
-        console.log('createDirectory', uri);
+    async createDirectory(uri) {
+        console.log(`playcanvas: createDirectory ${uri.path}`);
+        const project = this.getProject(uri.path);
+        const fileData = this.getFileData(uri);
+        if (!fileData) {
+            const folderPath = path.dirname(uri.path);
+            const folderUri = vscode.Uri.parse(`playcanvas:/${path.dirname(uri.path)}`);
+            
+            // Construct the new Uri using the folder path and new name
+            try {
+                const root = folderPath === `/${project.name}`;
+                const folderData = root ? null : this.getFileData(folderUri);
+                const name = uri.path.split('/').pop();
+                await this.api.createAsset(project.id, project.branchId, folderData ? folderData.id : null, name, 'folder');
+            } catch (error) {
+                vscode.window.showErrorMessage(`Failed to create a folder: ${error.message}`);
+            }
+        }
     }
 
     async fetchProjects() {
+        console.log(`playcanvas: fetchProjects`);
         if (!this.userId) {
             this.userId = await this.api.fetchUserId();
         }
@@ -153,6 +224,7 @@ class CloudStorageProvider {
     }
 
     async fetchBranches(project) {
+        console.log(`playcanvas: fetchBranches ${project.name}`);
         if (!project.branches) {
             const branches = await this.api.fetchBranches(project.id);
             project.branches = branches;
@@ -186,6 +258,7 @@ class CloudStorageProvider {
     }
 
     buildPaths(projectName, files) {
+        console.log(`playcanvas: buildPaths ${projectName}`);
         const fileMap = new Map();
         for (const file of files) {
             fileMap.set(file.id, file);
@@ -197,6 +270,7 @@ class CloudStorageProvider {
     }
 
     async fetchFiles(project) {
+        console.log(`playcanvas: fetchFiles ${project.name}`);
         if (!project.files) {
             const files = await this.api.fetchFiles(project.id, project.branchId);
             this.buildPaths(project.name, files);
@@ -213,11 +287,13 @@ class CloudStorageProvider {
     } 
 
     async fetchFileContent(fileData, branchId) {
+        console.log(`playcanvas: fetchFileContent ${fileData.name}`);
         return this.api.fetchFileContent(fileData.id, fileData.file.filename, branchId);
     }
 
     // key for the file map is branch id and filename
     getFileDataKey(uri) {
+        console.log(`playcanvas: getFileDataKey ${uri.path}`);
         const filename = uri.path;
         if (filename.length === 0) {
             return '';
@@ -248,8 +324,15 @@ class CloudStorageProvider {
         this.content = new Map();
     }
 
-    refreshProject(project) {
+    refreshUri(uri) {
+        // Fire the event to signal that a file has been changed.
+        // VS Code will call your readDirectory and other methods to update its view.
+        this._onDidChangeFile.fire([{ type: vscode.FileChangeType.Changed, uri: uri }]);
+    }
+
+    async refreshProject(project) {
         delete project.files;
+        await this.fetchFiles(project);
     }
 }
 
--- src/extension.js
+++ src/extension.js
@@ -1,14 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const TreeDataProvider = require('./treeDataProvider');
 const CloudStorageProvider = require('./cloudStorageProvider');
-const path = require('path');
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
-let copiedFile = null;
-let cutFile = null;
+async function selectProject(fileProvider) {
+	const projects = await fileProvider.fetchProjects();
+            
+	// find most recently modified 
+	projects.sort((a, b) => {
+		if (a.modified < b.modified) {
+			return 1;
+		}
+
+		if (a.modified > b.modified) {
+			return -1;
+		}
+
+		return 0;
+	});
+
+	if (projects.length === 0) {
+		return [];
+	}
+	const names = projects.map(project => project.name );
+	const project = await vscode.window.showQuickPick(names, { placeHolder: 'Select a project' });
+	if (project) {
+		return projects.find(p => p.name === project);
+	}
+}
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -17,55 +36,26 @@ async function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "playcanvas" is now active!');
+	console.log('playcanvas: Congratulations, your extension "playcanvas" is now active!');
 
 	try {
 
 		const fileProvider = new CloudStorageProvider();
 		context.subscriptions.push(vscode.workspace.registerFileSystemProvider('playcanvas', fileProvider, { isCaseSensitive: true }));
 
-		const treeDataProvider = new TreeDataProvider(fileProvider);
-		const treeView = vscode.window.createTreeView('PlayCanvasExplorer', { treeDataProvider: treeDataProvider });
+		await fileProvider.fetchProjects();
 
-		treeView.onDidChangeSelection(async e => {
-			try {
-				if (e.selection && e.selection.length > 0) {
-					const selectedItem = e.selection[0];
-					// Now you can do something with the selected item.
-					// For example, open it in a new editor tab:
-					if (!selectedItem.project && selectedItem.contextValue !== 'folder') {
-						const uri = vscode.Uri.parse(`playcanvas:/${selectedItem.path}`);
-						vscode.commands.executeCommand('vscode.open', uri);
-					}
-				}
-			} catch (error) {
-				console.log(error);
+		// Register a command to open a workspace that uses your file system provider
+		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.addProject', async (item) => {
+
+			const project = await selectProject(fileProvider);
+			if (project) {
+				vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0, 0, { uri: vscode.Uri.parse(`playcanvas:/${project.name}`), name: `${project.name}` });
 			}
-		});
-
-		treeView.onDidExpandElement(async e => {
-			treeDataProvider.onElementExpanded(e.element.path);
-		});
-
-		treeView.onDidCollapseElement(async e => {
-			treeDataProvider.onElementCollapsed(e.element);
-		});
-
-		context.subscriptions.push(treeView);
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.refresh', (item) => {
-			vscode.commands.executeCommand('workbench.actions.treeView.PlayCanvasExplorer.collapseAll');
-			fileProvider.refresh();
-			treeDataProvider.refresh();
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.collapseAll', (item) => {
-			vscode.commands.executeCommand('workbench.actions.treeView.PlayCanvasExplorer.collapseAll');
 		}));
 
 		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.switchBranch', async (item) => {
-			const projectUri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-			const project = await fileProvider.getProject(projectUri.path);
+			const project = await fileProvider.getProject(item.path);
 			const branches = await fileProvider.fetchBranches(project);
 
 			const prevBranch = project.branchId ? (branches.find( b => b.id === project.branchId )).name : 'main';
@@ -88,175 +78,11 @@ async function activate(context) {
 					vscode.window.showInformationMessage(`Switched to branch ${branch}`);
 					
 					// Refresh the tree view to reflect the file rename.
-					fileProvider.refreshProject(project);
-					treeDataProvider.refresh();
-
-					// refresh all currently opened files
-					vscode.commands.executeCommand('workbench.action.closeAllEditors');					
-					treeView.reveal(item, { select: true, focus: true });
+					await fileProvider.refreshProject(project);
+					vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
 				}				
 			}
 		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.newFile', async (item) => {
-			// Prompt the user for the new name.
-			const name = await vscode.window.showInputBox({
-				prompt: 'Enter the name'
-			});
-
-			if (name) {
-				const folderUri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-				
-				// Construct the new Uri using the folder path and new name
-				try {
-					await fileProvider.createFile(name, folderUri);
-					vscode.window.showInformationMessage('File created successfully');
-				} catch (error) {
-					vscode.window.showErrorMessage(`Failed to create a file: ${error.message}`);
-				}
-				
-				// Refresh the tree view to reflect the file rename.
-				fileProvider.refreshProject(fileProvider.getProject(folderUri.path));
-				treeDataProvider.refresh();		
-			}
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.newFolder', async (item) => {
-			// Prompt the user for the new name.
-			const name = await vscode.window.showInputBox({
-				prompt: 'Enter the name'
-			});
-
-			if (name) {
-				const folderUri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-				
-				// Construct the new Uri using the folder path and new name
-				try {
-					await fileProvider.createFile(name, folderUri, 'folder');
-					vscode.window.showInformationMessage('File created successfully');
-				} catch (error) {
-					vscode.window.showErrorMessage(`Failed to create a file: ${error.message}`);
-				}
-				
-				// Refresh the tree view to reflect the file rename.
-				fileProvider.refreshProject(fileProvider.getProject(folderUri.path));
-				treeDataProvider.refresh();		
-			}
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.copy', (item) => {
-			copiedFile = item.path;
-			cutFile = null;
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.cut', (item) => {
-			copiedFile = null;
-			cutFile = item.path;
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.paste', async (item) => {
-			const file = copiedFile || cutFile;
-			if (!file) {
-				vscode.window.showErrorMessage('No file copied');
-				return;
-			}
-
-			const uri = vscode.Uri.parse(`playcanvas:/${file}`);
-			const targetUri = vscode.Uri.parse(`playcanvas:/${item.path}/${path.basename(file)}`);
-			try {
-				// create a copy of the file in a new location
-				await vscode.workspace.fs.copy(uri, targetUri);
-
-				if (cutFile) {
-					// delete original file
-					await vscode.workspace.fs.delete(uri, {recursive: true, useTrash: false});
-				} 
-
-				vscode.window.showInformationMessage('File pasted successfully');
-			} catch (error) {
-				vscode.window.showErrorMessage(`Failed to paste file: ${error.message}`);
-				copiedFile = null;
-				cutFile = null;
-			}
-
-			// Refresh the tree view
-			if (cutFile) {
-				fileProvider.refreshProject(fileProvider.getProject(uri.path));
-			}
-			fileProvider.refreshProject(fileProvider.getProject(targetUri.path));
-			treeDataProvider.refresh();				
-
-			copiedFile = null;
-			cutFile = null;
-
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.rename', async (item) => {
-			// Prompt the user for the new name.
-			const newName = await vscode.window.showInputBox({
-				prompt: 'Enter the new name for the file'
-			});
-
-			if (newName) {
-				const uri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-				const dir = path.dirname(item.path);
-				
-				// Construct the new Uri using the old Uri and the new name.
-				const newUri = vscode.Uri.parse(`playcanvas:/${path.join(dir, newName)}`);
-				try {
-					await vscode.workspace.fs.rename(uri, newUri, { overwrite: false });
-					vscode.window.showInformationMessage('File renamed successfully');
-				} catch (error) {
-					vscode.window.showErrorMessage(`Failed to rename file: ${error.message}`);
-				}
-				
-				// Refresh the tree view to reflect the file rename.
-				fileProvider.refreshProject(fileProvider.getProject(uri.path));
-				treeDataProvider.refresh();		
-			}
-		}));
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.delete', async (item) => {
-			const uri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-
-			// Show a confirmation dialog
-			const confirm = await vscode.window.showWarningMessage('Are you sure you want to delete?', { modal: true }, 'Delete');
-
-			// If the user clicked 'Delete', delete the file
-			if (confirm === 'Delete') {
-
-				try {
-					await vscode.workspace.fs.delete(uri, {recursive: true, useTrash: false});
-					vscode.window.showInformationMessage('File deleted successfully');
-
-					// Refresh the tree view to reflect the file deletion.
-					fileProvider.refreshProject(fileProvider.getProject(uri.path));
-					treeDataProvider.refresh();
-				} catch (error) {
-					vscode.window.showErrorMessage(`Failed to delete file: ${error.message}`);
-				}
-			}
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.refreshFile', async (item) => {
-			const uri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-			const fileData = fileProvider.getFileData(uri);
-			const oldContent = new TextEncoder().encode(fileData.content);
-			if (fileData) {
-				delete fileData.content;
-			}
-			const newContent = await fileProvider.readFile(uri);
-			if (oldContent !== newContent) {
-				await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-				// Re-open the file to update the editor.
-				const document = await vscode.workspace.openTextDocument(uri);
-				vscode.window.showTextDocument(document);
-			}
-		}));
-
-		context.subscriptions.push(vscode.commands.registerCommand('playcanvas.openFile', (item) => {
-			const uri = vscode.Uri.parse(`playcanvas:/${item.path}`);
-			vscode.commands.executeCommand('vscode.open', uri);
-		}));						
 		
 	} catch (error) {
 		console.error('Failed to activate extension:', error);
