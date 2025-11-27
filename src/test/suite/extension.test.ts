5b8f5d6193473ba9f320e387487f46e872e3cd67
kpal <kpal81xd@gmail.com>
2025-11-27 17:52:01 +0000

    chore: renenabled override test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -407,7 +407,7 @@ suite('Extension Test Suite', () => {
     });
 
     // FIXME: fails as notification popup shows when override applied
-    test.skip('file changes (local overrides remote)', async () => {
+    test('file changes (local overrides remote)', async () => {
         const extension = vscode.extensions.getExtension('playcanvas.playcanvas');
         assert.ok(extension);
         await assertResolves(extension.activate(), 'extension.activate');
