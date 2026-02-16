4164053f7d411b48dd18322ae8fe6380e82737cc
KPal <48248865+kpal81xd@users.noreply.github.com>
2026-02-16 12:20:29 +0000

    feat: add line move op support from remote (#95)

--- src/typings/sharedb.d.ts
+++ src/typings/sharedb.d.ts
@@ -1,7 +1,9 @@
 export type ShareDbTextOp =
     | [string | { d: number }]
     | [number, string | { d: number }]
-    | [number, string, { d: number }];
+    | [number, string, { d: number }]
+    | [number, { d: number }, number, string]
+    | [number, string, number, { d: number }];
 
 export type ShareDbOp = {
     p: string[]; // path
